9e18b918f1b438a2db5d0f5caf8108271e3712bc
Pegah <43738935+pegahfallah@users.noreply.github.com>
2021-05-01 09:55:30 -0400

    Added description

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,58 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import Detector from '../pages-sections/index/detector';
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Detector from "../pages-sections/index/detector";
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   container: {},
 
-  main: { height: '60vh', background: '#273036', zIndex: '1' },
+  main: { height: "60vh", background: "#273036", zIndex: "1" },
   img: {
-    height: '500px',
-    borderRadius: '20px',
-    margin: '16px',
+    height: "500px",
+    borderRadius: "20px",
+    margin: "16px",
   },
   signup: {
-    position: 'absolute',
-    top: '35%',
-    marginLeft: '20%',
-    color: 'white',
-    fontFamily: 'Poppins',
-    zIndex: '1',
+    position: "absolute",
+    top: "35%",
+    marginLeft: "20%",
+    color: "white",
+    fontFamily: "Poppins",
+    zIndex: "1",
+  },
+  subtitle: {
+    width: "80%",
+    margin: "auto",
   },
   h1: {
-    fontSize: '4em',
+    fontSize: "4em",
   },
   imgContainer: {
-    width: '100%',
-    margin: 'auto',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: "100%",
+    margin: "auto",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
 
-  icon: { height: '200px', borderRadius: '10px' },
+  icon: { height: "200px", borderRadius: "10px" },
   iconContainer: {
-    position: 'absolute',
-    top: '38%',
-    marginLeft: '6%',
+    position: "absolute",
+    top: "38%",
+    marginLeft: "6%",
   },
   btn: {
-    '&hover': {
-      color: 'red',
+    "&hover": {
+      color: "red",
     },
-    background: 'white',
-    border: 'none',
-    padding: '16px',
-    borderRadius: '25px',
+    background: "white",
+    border: "none",
+    padding: "16px",
+    borderRadius: "25px",
   },
 }));
 
@@ -64,31 +68,46 @@ export default function Index() {
         </div>
         <div className={classes.iconContainer}>
           <img
-            src='https://upload.wikimedia.org/wikipedia/commons/d/d4/Sustainable_Development_Goal_10.png'
+            src="https://upload.wikimedia.org/wikipedia/commons/d/d4/Sustainable_Development_Goal_10.png"
             className={classes.icon}
           ></img>
         </div>
       </div>
-      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path
-          fill='#273036'
-          fill-opacity='1'
-          d='M0,320L60,293.3C120,267,240,213,360,170.7C480,128,600,96,720,117.3C840,139,960,213,1080,240C1200,267,1320,245,1380,234.7L1440,224L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z'
+          fill="#273036"
+          fill-opacity="1"
+          d="M0,320L60,293.3C120,267,240,213,360,170.7C480,128,600,96,720,117.3C840,139,960,213,1080,240C1200,267,1320,245,1380,234.7L1440,224L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
         ></path>
       </svg>
       <div className={classes.imgContainer}>
         <img
           className={classes.img}
-          src='https://images.pexels.com/photos/4629633/pexels-photo-4629633.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+          src="https://images.pexels.com/photos/4629633/pexels-photo-4629633.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
         ></img>
         <img
           className={classes.img}
-          src='https://images.pexels.com/photos/4629634/pexels-photo-4629634.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+          src="https://images.pexels.com/photos/4629634/pexels-photo-4629634.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
         ></img>
         <img
           className={classes.img}
-          src='https://images.pexels.com/photos/4629625/pexels-photo-4629625.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+          src="https://images.pexels.com/photos/4629625/pexels-photo-4629625.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
         ></img>
+      </div>
+      <div className={classes.subtitle}>
+        <h1>How can innovative technologies promote reduced inequalities?</h1>
+        <p>
+          Applications and software technologies have now become essential and
+          our dependency on them is increasing exponentially each year. In this
+          project, the student team will understand the knowledge, skills, and
+          practices of web development and Machine Learning and get hands-on
+          experience working with the intrinsic components (front-end, backend,
+          system design, and the UI) of an infrastructure to gain insight on how
+          software and applications work. The project team will then develop a
+          machine learning based prototype for deaf and people who are hard of
+          hearing, the model will help them in communicating effectively thereby
+          cultivating EDI (Equity, Diversity & Inclusivity) in society.
+        </p>
       </div>
     </div>
   );
